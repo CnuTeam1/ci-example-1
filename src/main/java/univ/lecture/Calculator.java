420229f3035883a6fded86cc1ba156f1e8d5ffc2
Joh <jms5049@naver.com>
2017-04-06 00:50:46 +0900

    infixToPostfix : calculating the size of Array

--- src/main/java/univ/lecture/Calculator.java
+++ src/main/java/univ/lecture/Calculator.java
@@ -5,7 +5,7 @@ package univ.lecture;
  * 201302493 최기현
  * 201302487 조민성
  */
-
+import java.util.Stack;
 /**
  * Created by tchi on 2017. 3. 19..
  * 
@@ -45,7 +45,27 @@ public class Calculator {
 
 
 		private String[] infixToPostfix(String[] args) {
-		
+			private Stack<String> stack;
+			// private ArrayStack stack;
+
+			private String[] infixToPostfix(String[] args) {
+				// 중위식을 후위식으로 변환한 결과를 String 배열로 반환하는 코드를 작성하고,
+				// 마지막에 변환된 후위식을 출력함.
+				stack = new Stack<String>();
+				// stack = new ArrayStack(args.length);
+				int count = 0;
+				int size = 0;
+
+				// *************** 스트링 배열의 크기를 찾기위한 코드 **************************
+				for (int i = 0; i < args.length; i++) {
+					if ("()".indexOf(args[i]) >= 0) {
+						size++;
+					}
+				}
+				size = args.length - size;
+				String[] a = new String[size];
+				// ***********************************************************
+	
 		}
 
 		private int precedence(String token) {
