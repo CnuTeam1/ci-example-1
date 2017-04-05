7e0077c93451063030bf6efe8255de60efa4359c
Jaesung Chris NA <na-0003@hotmail.com>
2017-04-05 23:53:17 +0900

    skeleton code

--- src/main/java/univ/lecture/Calculator.java
+++ src/main/java/univ/lecture/Calculator.java
@@ -1,10 +1,49 @@
 package univ.lecture;
 
+
 /**
  * Created by tchi on 2017. 3. 19..
  */
 public class Calculator {
-    public int calculate(String exp) {
-        return 2;
-    }
+	public int calculate(String exp) {
+
+		return 2;
+	}
+
+	private String[] expressionTokenizer(String exp) {
+
+	}
+
+
+
+	private class RPN {
+
+		private RPN(String[] args) {
+
+		}
+		private String returnResult(){
+		
+		}
+		private boolean isAnOperator(String s) {
+
+		}
+
+		private double evaluate(double x, double y, String op) {
+
+		}
+
+	}
+
+	private class Transform {
+
+
+		private String[] infixToPostfix(String[] args) {
+		
+		}
+
+		private int precedence(String token) {
+		
+		}
+	}
+
 }
